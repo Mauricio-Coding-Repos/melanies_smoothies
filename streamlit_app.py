38dc479e76f14131096ad441f809457e4b8c1aa6
Mauricio-Coding-Repos <mauriciosolis619@gmail.com>
2025-08-31 00:49:45 -0600

    Update streamlit_app.py with display information
    It was added a import request in the new section at the end. It also added smoothiefroot_response request object.

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+
+# New Section to display smothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
